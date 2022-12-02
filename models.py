cbb128d33c6a21938d755cbd2869ac08661ff983
ailiharmon <89175417+ailiharmon@users.noreply.github.com>
2022-12-02 15:21:58 -0600

    added GAT model

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GATConv
 
 # Multi Layer GCN regression model
 class MultiLayerGCNNet(torch.nn.Module):
@@ -27,3 +28,17 @@ class MultiLayerGCNNet(torch.nn.Module):
         for i in range(1, self.L - 1):
             h = F.relu(self.model[i](h, edge_index))  # relu applied
         return self.model[-1](h, edge_index)  # no relu
+
+class GATNet(torch.nn.Module):
+    def __init__(self, D_in, H, L):
+ 
+        super(GATNet, self).__init__()
+        self.conv1 = GATConv(D_in, H)
+        self.conv2 = GATConv(H, 1)
+
+    def forward(self, data):
+
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
